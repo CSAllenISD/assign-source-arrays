3e63115ca4a1aa29327b6fe400f04be921d69f10
David Ben-Yaakov0 <david@runningwind.com>
2018-04-02 18:35:49 -0500

    First version

---
+++ README.md
@@ -0,0 +1,5 @@
+# Computer Science III, Period 5
+# John Williams
+# Arrays
+# 2-April-2018
+
---
+++ arrays.swift
@@ -0,0 +1,142 @@
+// Computer Science III, Period 5
+// John Williams
+// Arrays
+// 2-April-2018
+
+
+
+/********************************************************************************
+|  1. Prompt the user to enter an arbitrary number of integers, one per line
+|     Inform the user to terminate the input with a ^D (the end-of-file indicator)
+*********************************************************************************/
+
+
+
+/********************************************************************************
+|  2. Provide a prompt to the user and have them enter an integer ON THE SAME LINE
+|     as the prompt.
+|     If the user entered an integer add it to the END of an array.
+|     Continue to prompt until EOF is received.
+********************************************************************************/
+
+
+/********************************************************************************
+| NOTE:  For the remainder of this lab, if the order of traversal of the array
+|        is not specified, assume forward order.
+********************************************************************************/
+
+
+/********************************************************************************
+|  3.  Print "A: " and then the largest integer in the array, all on the same line
+********************************************************************************/
+
+
+
+/********************************************************************************
+|  4.  Print "B: " and then the smallest integer in the array, 
+|      all on the same line
+********************************************************************************/
+
+
+
+/********************************************************************************
+|  5.  Print "C: " and then all negative integers in the array, all on the same 
+|      line
+********************************************************************************/
+
+
+
+/********************************************************************************
+|  6.  Print "D: " and then all integers at odd indices in the array, all on the 
+|      same line
+********************************************************************************/
+
+
+
+/********************************************************************************
+|  7.  Print "E: " and then all negative integers at even indices in the array, 
+|      all on the same line
+********************************************************************************/
+
+
+
+/********************************************************************************
+|  8.  Print "F: " and then all negative integers in the array, in reverse order,
+|      all on the same line
+********************************************************************************/
+
+
+
+/********************************************************************************
+|  9.  Print "G: " and then every even number which occurs immediately after
+|      an odd number, all on the same line
+********************************************************************************/
+
+
+
+/********************************************************************************
+| 10.  Print "H: " and then every even number which occurs immediately BEFORE
+|      an odd number, all on the same line
+********************************************************************************/
+
+
+/********************************************************************************
+| 11.  Print "I: " and then every number which is larger than its 
+|      predecessor.  (The first number in the array should not be printed.)
+********************************************************************************/
+
+
+
+/********************************************************************************
+| 12.  Print "J: " and then every number which is larger than its 
+|      successor.  (The last number in the array should not be printed.)
+********************************************************************************/
+
+
+
+/********************************************************************************
+| 13.  Print "K: " and then every number which is the sum of the two prior
+|      numbers, all on the same line.
+********************************************************************************/
+
+
+
+/********************************************************************************
+| 14.  Print "L: " and then every number which is the sum of the two successive
+|      numbers, all on the same line.
+********************************************************************************/
+
+
+
+/********************************************************************************
+| 15.  Print "M: " and then the second largest integer in the array,
+|      all on the same line.
+********************************************************************************/
+
+
+
+/********************************************************************************
+| 16.  Print "N: " and then the sum of all odd integers in the array,
+|      all on the same line.
+********************************************************************************/
+
+
+
+/********************************************************************************
+| 17.  Print "O: " and then the COUNT of all odd integer in the array followed
+|      by the COUNT of all even integers in the array, all on the same line.
+********************************************************************************/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
